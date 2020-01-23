8864e7ca749ad4cc1e38b3bee2c74a2250aa106f
Ebrar Islami <ebrar.islami@technoperia.com>
2020-01-23 15:42:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import sculptureVideo from "./videos/sculpture.mp4";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const url = "https://orangevalleycaa.org/api/videos";
+    const fetchData = async () => {
+      const result = await fetch(url).then(response => response.json());
+      setData(result);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="">
+        <h1>Videos</h1>
       </header>
+
+      <div>
+        <video src={sculptureVideo} controls height={200}></video>
+      </div>
+
+      {data.map(video => (
+        <div key={video.id}>
+          <h2>{video.name}</h2>
+          <video height={200} controls src={video.video_url}></video>
+        </div>
+      ))}
     </div>
   );
 }
